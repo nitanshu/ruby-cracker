bb3f88f6f37711272235dfd4e5e1beb08b69434d
nitanshu <niverma@q3tech.com>
2018-08-06 12:25:07 +0530

    define method file added

---
+++ define_method.rb
@@ -0,0 +1,9 @@
+class Car
+  FEATURES = %w(engine wheel airbag alarm stereo)
+  FEATURES.each do |feature|
+    define_method("#{feature}_info=") do |info|
+      instance_variable_set("@#{feature}_info", info)
+    end
+  end
+end
+
