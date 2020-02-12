c380894cd3e4fb0df25af9963a60a2e98246eab1
nitanshu <nitanshu.verma@mind-infotech.com>
2020-02-12 22:02:58 +0530

    added remove_methd

--- Meta_Ruby_Pablo_Perotta/Tuesday_methods/remove_method.rb
+++ Meta_Ruby_Pablo_Perotta/Tuesday_methods/remove_method.rb
@@ -1,19 +1,28 @@
-class String
-  def reverse
-    'I am only removed from this class'
-  end
+  # undef_method Prevents the current class from responding to calls to the named method.
+  # Contrast this with remove_method, which deletes the method from the particular class;
+  # Ruby will still search superclasses and mixed-in modules for a possible receiver.
+  # String arguments are converted to symbols.
 
-  remove_method :reverse
+class Parent
+  def hello
+    puts "In parent"
+  end
+end
+class Child < Parent
+  def hello
+    puts "In child"
+  end
 end
 
-"nitanshu".reverse
+c = Child.new
+c.hello
 
-class String
-  def reverse
-    'I am prevented to look up in the ancestor chain'
-  end
-
-  undef_method :reverse
+class Child
+  remove_method :hello  # remove from child, still in parent
 end
+c.hello
 
-#TODO:  BLOG NEEDS TO WRITE ON THIS 
+class Child
+  undef_method :hello   # prevent any calls to 'hello'
+end
+c.hello
