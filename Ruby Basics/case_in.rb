0261614960b244fa0d2839b0fddf059e93e9f684
Nitanshu Verma <nitanshu.verma@careerbuilder.com>
2023-06-14 14:27:04 -0500

    vs code configuration

---
+++ Ruby Basics/case_in.rb
@@ -0,0 +1,9 @@
+response = { error: 'Bad Gateway', code: 502 }
+case response
+in { data: data, code: code }
+  puts "Success #{data}, Code: #{code}"
+in { error: error, code: code }
+  puts "Error: #{error}, Code: #{code}"
+end
+
+#Error: Bad Gateway, Code: 502
---
+++ Ruby Basics/nested_class.rb
@@ -0,0 +1,19 @@
+class Drawing
+    def self.give_me_a_circle
+      Circle.new
+    end
+    class Line
+    end
+    class Circle
+      def what_am_i
+        "This is a circle"
+      end
+  end 
+end
+
+a = Drawing.give_me_a_circle
+puts a.what_am_i
+b = Drawing::Circle.new
+puts b.what_am_i
+c = Circle.new
+puts c.what_am_i
